bdc0d7e77717ad605ab7bb471ff65930af022c31
Chelsea Norrgård <searenee@gmail.com>
2025-02-18 11:35:54 +0200

    Animations
    Player and idle/animations added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movementSpeed;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private SpriteRenderer _characterBody;
+    [SerializeField] private float _movementSpeed;
     private Rigidbody2D _rb;
 
     void Start()
@@ -21,7 +23,13 @@ public class PlayerController : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized * movementSpeed;
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized * _movementSpeed;
         _rb.velocity = movement;
+
+        bool characterIsWalking = movement.magnitude > 0f;
+        _animator.SetBool("isWalking", characterIsWalking);
+
+        bool flipSprite = movement.x < 0f;
+        _characterBody.flipX = flipSprite;
     }
 }
